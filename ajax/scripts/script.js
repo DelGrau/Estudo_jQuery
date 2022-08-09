9418bd3713539ec5a8e30237a19d847400eec06a
Matheus Soares <math.fssoares@gmail.com>
2022-08-09 18:02:21 -0300

    Criada DB_paises para uma futura consulta de paises para gerar os dados do tempo

--- ajax/scripts/script.js
+++ ajax/scripts/script.js
@@ -1,8 +1,13 @@
+
+
 $(document).ready(function() {
     $('#botao').click(function() {
         $('#teste').load('texto.html')
     })
 
+
+    //METODO BLUR PARA ASSIM QUE FOR TIRADO O FOCO DO ELEMENTO O SEU VALOR
+    //É ARMAZENADO
     $('#cep').blur(function() {
         var numero_cep = $(this).val(); 
         
@@ -12,6 +17,7 @@ $(document).ready(function() {
             if(status == 'success'){
                 $('#cidade').val(dados.localidade)
                 $('#uf').val(dados.uf)
+                $('#endereco').val(dados.logradouro)
             }else {
                 alert('Algo deu errado com a Requisição!')
             }
@@ -19,6 +25,8 @@ $(document).ready(function() {
 
     })
 
+
+    //FILTRO DE BUSCA EM ITENS DE LISTA OU TABELAS HTML
     $('#busca').keyup(function (){
         var valor_busca = $(this).val().toLowerCase()
         //Passamos o ID e qual o elemento vamos fazer a procura
@@ -29,6 +37,22 @@ $(document).ready(function() {
 
 
     
+    //PEGANDO INFORMAÇÕES DA API DE CLIMA  
+    $.get('https://api.openweathermap.org/data/2.5/weather?lat=-23.5489&lon=-46.6388&lang=pt_br&appid=069bf7ad6a29404c5644b4709aa6f692&units=metric', function(data, status){
+        console.log(data)
+
+       
+        weather = data.weather
+        console.log(weather[0].description)
+
+        $("#busca").val(weather[0].description)
+        console.log(data.name)
+        
+
+       
+
+
+    })
 
 })
 
