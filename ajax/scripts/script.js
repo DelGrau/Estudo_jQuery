04594d93988b60464efd8fd867e63db28cb24b87
Matheus Soares <math.fssoares@gmail.com>
2022-08-09 21:44:28 -0300

    Adicionada a Datalist com os paises, preparando para pegar o nome dos paises e retornar os dados

--- ajax/scripts/script.js
+++ ajax/scripts/script.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
     //É ARMAZENADO
     $('#cep').blur(function() {
         var numero_cep = $(this).val(); 
-        
+        console.log(numero_cep )
         //metodo do tipo get para pegar valores de uma API externa
        $.get('https://viacep.com.br/ws/'+numero_cep+'/json/', function(dados, status) {
             console.log(dados)
@@ -34,25 +34,44 @@ $(document).ready(function() {
             $(this).toggle($(this).text().toLocaleLowerCase().indexOf(valor_busca) > -1)
         })
     })
+    var lat = 50
+    var log = 50
+    $('#lat').blur(function() {
+        lat = $(this).val()
+        pesquisarClima()
+    })
+    $('#log').blur(function(){
+        log = $(this).val()
+        pesquisarClima()
+    })
+     
 
-
+    //console.log(lat)
+    console.log(log)
     
-    //PEGANDO INFORMAÇÕES DA API DE CLIMA  
-    $.get('https://api.openweathermap.org/data/2.5/weather?lat=-23.5489&lon=-46.6388&lang=pt_br&appid=069bf7ad6a29404c5644b4709aa6f692&units=metric', function(data, status){
-        console.log(data)
-
-       
+    //PEGANDO INFORMAÇÕES DA API DE CLIMA 
+    function pesquisarClima(){
+        $.get('https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+log+'&lang=pt_br&appid=069bf7ad6a29404c5644b4709aa6f692&units=metric', function(data, status){       
         weather = data.weather
         console.log(weather[0].description)
-
+        var nome = String(data.name)
         $("#busca").val(weather[0].description)
-        console.log(data.name)
-        
+        $('#city').val(data.name)
 
-       
+        })
+    } 
 
+    let lista_paises
+    $.get('http://127.0.0.1:5500/ajax/DB_paises/city.list.json.log', function(data, status){
+        lista_paises = JSON.parse(data)
+        console.log(lista_paises.length)  
+           
+        for(var i =0; i < lista_paises.length; i++){
+            $('#contry').append('<option value='+lista_paises[i].name.replace(" ", "_")+'></option>')
+        }  
 
     })
+
 
 })
 
